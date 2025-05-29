662443eea669d8b0b098d28eeaba10f6226abe7b
Abhishek Jacob Santhosh <abhisheksanthosh45@gmail.com>
2025-05-29 10:11:23 +0530

    Create allheaders.h

---
+++ lab_exam/allheaders.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<signal.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<errno.h>
+#include<time.h>
+#include<dirent.h>
+#include<utime.h>
